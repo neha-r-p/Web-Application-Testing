7f8c062365278ecba73ce6c744d6e24b1f1a0407
Neha Patel <neha.r.p@gmail.com>
2019-08-08 15:27:12 -0700

    render dashboard test

--- web-app-testing/src/Dashboard.test.js
+++ web-app-testing/src/Dashboard.test.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import { render, cleanup, fireEvent } from "@testing-library/react";
+import Dashboard from './Dashboard'
+
+describe("<Dashboard />", () =>  {
+    it('renders without crashing', () => {
+        render(<Dashboard />)
+    })
+})
